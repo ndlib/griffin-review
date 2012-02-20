59de9b597ced15676a6e2cf714aefa4ff74d9290
= <=>
2012-02-20 16:19:41 -0500

    Initial structure

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Griffin::Application.config.secret_token = 'b1adefb2743d9f2a655414f3584116cedae3d3bcf88fce39bf1ff0a0a08cc082e970d16a7f57409b6bb7498fc7b91d95fc652f0309e696443b464adb86093cb9'
