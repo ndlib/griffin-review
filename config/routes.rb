d978f2bd23895e7b3941c13226967b034c28a8ca
Robert Fox <rfox2@nd.edu>
2012-12-06 11:56:50 -0500

    Minor clean up on glyph images

--- config/routes.rb
+++ config/routes.rb
@@ -15,6 +15,7 @@ Griffin::Application.routes.draw do
     resources :video
   end
   match '/img/glyphicons-halflings.png' => redirect('/assets/img/glyphicons-halflings.png')
+  match '/img/glyphicons-halflings-white.png' => redirect('/assets/img/glyphicons-halflings-white.png')
   scope '/admin/video/request', :controller => 'admin/video_workflow' do
     get 'requests_by_semester', :as => nil, :path => '/by_semester/:s_id'
     post 'state', :action => 'requests_by_state', :as => 'requests_by_state'
