409f7f5ce97fe920d3c13240e659f45c8f5c8812
Jonathan Hartzler <Jonathan.Hartzler.3@nd.edu>
2013-07-03 09:58:15 -0400

    added andy as an admin.

--- app/models/permission.rb
+++ app/models/permission.rb
@@ -6,7 +6,7 @@ class Permission
 
 
   def current_user_is_administrator?
-    ['rfox2', 'jhartzle'].include?(@current_user.username)
+    ['rfox2', 'jhartzle', 'fboze'].include?(@current_user.username)
   end
 
 
