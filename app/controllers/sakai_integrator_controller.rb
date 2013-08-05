d85163aa7f3c42fb087af3c8cf6c448543c43c72
Robert Fox <robertjonfox@gmail.com>
2013-08-05 15:42:18 -0400

    Add conditional to catch enrollment validation.

--- app/controllers/sakai_integrator_controller.rb
+++ app/controllers/sakai_integrator_controller.rb
@@ -6,7 +6,11 @@ class SakaiIntegratorController < ApplicationController
       si.site_id = params[:context_id]
       external_site_id = si.get_site_property('externalSiteId')
       section_group_id = si.translate_external_site_id(external_site_id)
-      redirect_to course_reserves_path(:course_id => section_group_id)
+      if !section_group_id.blank?
+        redirect_to course_reserves_path(:course_id => section_group_id)
+      else
+        render_404
+      end
     else
       render_404
     end
