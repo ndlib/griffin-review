6f0762e3e981241c226d8140cf3c63db299119ee
Jonathan Hartzler <Jonathan.Hartzler.3@nd.edu>
2013-08-09 15:48:50 -0400

    fix the unique constraint on emails in the users table.

--- db/migrate/20120618210515_devise_create_users.rb
+++ db/migrate/20120618210515_devise_create_users.rb
@@ -14,7 +14,7 @@ class DeviseCreateUsers < ActiveRecord::Migration
       t.timestamps
     end
 
-    add_index :users, :email, :unique => true
+    add_index :users, :email
     add_index :users, :username, :unique => true
   end
 end
