100faeb8ee58cbf30b5916d671f7879fc0070912
Jonathan Hartzler <Jonathan.Hartzler.3@nd.edu>
2013-08-14 10:27:29 -0400

    forgot to add the specific class!

---
+++ app/models/active_record/reserve_stat.rb
@@ -0,0 +1,26 @@
+
+
+class ReserveStat < ActiveRecord::Base
+  belongs_to :user
+  belongs_to :request
+  belongs_to :item
+  belongs_to :semester
+
+
+  def self.add_statistic!(user, reserve)
+    ret = self.new( user_id: user.id, request_id: reserve.request.id, item_id: reserve.item.id, semester_id: reserve.semester.id)
+    ret.save!
+    ret
+  end
+
+
+  def self.all_request_stats(reserve)
+    self.where(request_id: reserve.request.id)
+  end
+
+
+  def self.all_item_stats(reserve)
+    self.where(item_id: reserve.item.id)
+  end
+
+end
