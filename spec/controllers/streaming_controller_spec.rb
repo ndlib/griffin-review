4e700621ce28ef4a0c1d6c6d73a291f379fd0aee
Robert Fox <robertjonfox@gmail.com>
2013-08-27 15:40:53 -0400

    Updating streaming spec to be compatible with VCR set up.

--- spec/controllers/streaming_controller_spec.rb
+++ spec/controllers/streaming_controller_spec.rb
@@ -20,6 +20,11 @@ describe StreamingController do
     GetReserve.any_instance.stub(:get_course_token).and_return('token')
   end
 
+  it "does not allow you to view the item if the id is not in the course id" do
+    expect {
+      get :show, token: 'token', id: 'id', course_id: "not course id "
+    }.to render_template(nil)
+  end
 
   it "allows you in if the token is valid and there is no session " do
     get :show, token: 'token', id: 'id', course_id: @current_course.id
@@ -34,7 +39,7 @@ describe StreamingController do
 
     expect {
       get :show, token: 'token', id: 'id', course_id: @current_course.id
-    }.to raise_error ActionController::RoutingError
+    }.to render_template(nil)
   end
 
 
