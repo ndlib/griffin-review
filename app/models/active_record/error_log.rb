398029f9429d59aadbf9db5ed07e1f3742a14e87
Robert Fox <robertjonfox@gmail.com>
2013-09-04 16:51:02 -0400

    Store backtrace as crlf delimited and order errors desc.

--- app/models/active_record/error_log.rb
+++ app/models/active_record/error_log.rb
@@ -1,12 +1,12 @@
 class ErrorLog  < ActiveRecord::Base
 
-  scope :default_order, -> { order(:created_at) }
+  scope :default_order, -> { order("created_at DESC") }
 
   def self.log_error(current_user, request, exception, masquerading_user = nil)
     exception_message = exception.blank? ? 'Nil Exception' : exception.message
     current_username = current_user.present? ? current_user.username : 'Unknown User'
     current_username = masquerading_user.blank? ? current_user.username : "#{masquerading_user.username} (as: " + current_user.username + ")"
-    exception_backtrace = exception.blank? ? 'Nil Exception' : exception.backtrace.to_s
+    exception_backtrace = exception.blank? ? 'Nil Exception' : exception.backtrace.join("\n")
     ErrorLog.create(message: exception_message, netid: current_username, path: request.path, params: request.params.to_s, stack_trace: exception_backtrace )
   end
 
