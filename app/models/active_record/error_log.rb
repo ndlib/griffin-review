b254e4bb37907da602d7bc3862d0eb026355393e
Jonathan Hartzler <Jonathan.Hartzler.3@nd.edu>
2013-09-06 11:31:30 -0400

    fix the error log errors.

--- app/models/active_record/error_log.rb
+++ app/models/active_record/error_log.rb
@@ -2,12 +2,14 @@ class ErrorLog  < ActiveRecord::Base
 
   scope :default_order, -> { order("created_at DESC") }
 
-  def self.log_error(current_user, request, exception, masquerading_user = nil)
-    exception_message = exception.blank? ? 'Nil Exception' : exception.message
-    current_username = current_user.present? ? current_user.username : 'Unknown User'
-    current_username = masquerading_user.blank? ? current_user.username : "#{masquerading_user.username} (as: " + current_user.username + ")"
-    exception_backtrace = exception.blank? ? 'Nil Exception' : exception.backtrace.join("\n")
-    ErrorLog.create(message: exception_message, netid: current_username, path: request.path, params: request.params.to_s, stack_trace: exception_backtrace )
+  def self.log_error(current_user, request, exception, masquerading_user = "")
+    ErrorLog.create(
+      message: determine_message(exception),
+      netid: determine_netid(current_user, masquerading_user),
+      path: request.path,
+      params: request.params.to_s,
+      stack_trace: determine_backtrace(exception)
+    )
   end
 
 
@@ -16,4 +18,29 @@ class ErrorLog  < ActiveRecord::Base
   end
 
 
+  def self.determine_netid(current_user, masquerading_user)
+    if current_user.present?
+      masquerading_user.blank? ? current_user.username : "#{masquerading_user.username} (as: " + current_user.username + ")"
+    else
+      "Uknown User"
+    end
+  end
+
+
+  def self.determine_message(exception)
+    exception.blank? ? 'Nil Exception' : exception.message
+  end
+
+
+  def self.determine_backtrace(exception)
+
+    if exception.blank?
+      'Nil Exception'
+    elsif exception.backtrace.nil?
+      "No Backtrace"
+    else
+      exception.backtrace.join("\n")
+    end
+
+  end
 end
--- lib/error_helper.rb
+++ lib/error_helper.rb
@@ -1,5 +1,5 @@
 module ErrorHelper
-  
+
   def catch_404(exception=nil)
     @masquerading_user = log_error(exception)
     respond_to do |format|
@@ -15,23 +15,34 @@ module ErrorHelper
     end
   end
 
+
   def log_error(exception)
     @e = env["action_dispatch.exception"]
     masquerading_user = determine_masquerade
     if exception.blank?
       ErrorLog.log_error(current_user, request, @e, masquerading_user)
-      Rails.logger.warn(DateTime.now.to_s + " [ERROR RAISED] #{@e.class.to_s} #{@e.message} " + current_user.username)
+      Rails.logger.warn(DateTime.now.to_s + " [ERROR RAISED] #{@e.class.to_s} #{@e.message} #{determine_netid}")
     else
       ErrorLog.log_error(current_user, request, exception, masquerading_user)
-      Rails.logger.warn(DateTime.now.to_s + " [ERROR RAISED] #{exception.inspect} " + current_user.username)
+      Rails.logger.warn(DateTime.now.to_s + " [ERROR RAISED] #{exception.inspect} #{determine_netid}")
     end
     return masquerading_user
   end
+
 
   def determine_masquerade
     masquerade = Masquerade.new(self)
     if masquerade.masquerading?
       masquerade.original_user
+    end
+  end
+
+
+  def determine_netid
+    if current_user.present?
+      masquerading_user.blank? ? current_user.username : "#{masquerading_user.username} (as: " + current_user.username + ")"
+    else
+      "Uknown User"
     end
   end
 
