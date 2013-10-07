390c19b3783dc5e86c05191972e172924add4eaa
Jonathan Hartzler <Jonathan.Hartzler.3@nd.edu>
2013-10-07 11:18:57 -0400

    added permission checks to the users.

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -2,26 +2,34 @@ class UsersController < ApplicationController
 
 
   def index
+    check_admin_or_admin_masquerading_permission!
     @users = User.where(admin: true)
   end
 
 
   def new
+    check_admin_or_admin_masquerading_permission!
+
     @user = User.find(params[:id])
   end
 
 
   def create
-
+    check_admin_or_admin_masquerading_permission!
   end
 
+
   def edit
+    check_admin_or_admin_masquerading_permission!
+
     @user = User.find(params[:id])
     @filter = RequestFilter.new(self, @user)
   end
 
 
   def update
+    check_admin_or_admin_masquerading_permission!
+
     @user = User.find(params[:id])
     filter = RequestFilter.new(self, @user)
 
