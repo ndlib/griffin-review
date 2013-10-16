9622626d825e544e93ea5fc3b239ad13bfac898c
Jonathan Hartzler <Jonathan.Hartzler.3@nd.edu>
2013-10-16 09:46:56 -0400

    removed unused code.

--- app/models/active_record/basic_metadata.rb
+++
@@ -1,9 +0,0 @@
-class BasicMetadata < ActiveRecord::Base
-#  attr_accessible :item_id, :metadata_attribute_id, :value
-
-  belongs_to :item
-  belongs_to :metadata_attribute
-
-  validates :value, :presence => true
-
-end
--- app/models/active_record/item_type.rb
+++
@@ -1,7 +0,0 @@
-class ItemType < ActiveRecord::Base
-#  attr_accessible :description, :name
-
-  validates_presence_of :name, :description
-
-  has_many :items
-end
