373984b38ac405764268ea4fec4b245808ad6f5d
Jonathan Hartzler <Jonathan.Hartzler.3@nd.edu>
2013-10-22 16:07:10 -0400

    make the instructor forms look better.

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -19,8 +19,12 @@ module ApplicationHelper
   end
 
 
-  def library_select_form(f)
-    f.input :library, as: "select", collection: { 'Hesburgh Library' => :hesburgh , 'O\' Meara Mathmatics Library' => :math, 'Chemestry - Physics Library' => :chem, 'Mahaffey Business Library' => :business, 'Architecture Library' => :architecture, 'Engeneering Library' => :engeneering }, :selected => 'Hesburgh'
+  def library_select_form(f, hint = "")
+    f.input :library,
+      as: "select",
+      collection: { 'Hesburgh Library' => :hesburgh , 'O\' Meara Mathmatics Library' => :math, 'Chemestry - Physics Library' => :chem, 'Mahaffey Business Library' => :business, 'Architecture Library' => :architecture, 'Engeneering Library' => :engeneering },
+      :selected => 'Hesburgh',
+      hint: hint
   end
 
 
