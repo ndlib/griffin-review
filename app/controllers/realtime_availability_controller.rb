dec718cf563a9118b13806f97b4b64b2b1bd7a2c
Jonathan Hartzler <Jonathan.Hartzler.3@nd.edu>
2013-10-30 12:47:39 -0400

    fix my error.

--- app/controllers/realtime_availability_controller.rb
+++ app/controllers/realtime_availability_controller.rb
@@ -5,6 +5,5 @@ class RealtimeAvailabilityController < ApplicationController
   def show
     params[:key] = (0...50).map{ ('a'..'z').to_a[rand(26)] }.join
     @rta = Rta.new(params[:id], params[:key])
-    raise "hi"
   end
 end
