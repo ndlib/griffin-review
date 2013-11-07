1ab05eca3de61f9ddcaa8cab4bb22ee93c22ebe2
Jonathan Hartzler <Jonathan.Hartzler.3@nd.edu>
2013-11-07 14:15:27 -0500

    fix tests and simplify files structure

--- config/initializers/date_time_formats.rb
+++ config/initializers/date_time_formats.rb
@@ -1,2 +1,4 @@
 Date::DATE_FORMATS[:default] = '%m/%d/%Y'
 Time::DATE_FORMATS[:default]= '%m/%d/%Y %I:%M%p'
+
+Time::DATE_FORMATS[:short_ordinal] = lambda { |time| time.strftime("%B #{time.day.ordinalize}") }
