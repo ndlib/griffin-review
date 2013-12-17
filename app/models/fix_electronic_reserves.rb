afb4b787d6d406844546b1db7fb7f7ceed2d77d0
Jonathan Hartzler <Jonathan.Hartzler.3@nd.edu>
2013-12-17 14:03:21 -0500

    adjust the fix for the type.

--- app/models/fix_electronic_reserves.rb
+++ app/models/fix_electronic_reserves.rb
@@ -7,8 +7,8 @@ class FixElectronicReserves
     Item.update_all('physical_reserve = 0, electronic_reserve = 1', 'type = "JournalReserve"')
 
 
-    Item.update_all('physical_reserve = 1, electronic_reserve = 0', 'type = "VideoReserve" AND physical_reserve = 1 ')
-    Item.update_all('physical_reserve = 1, electronic_reserve = 0', 'type = "AudioReserve" AND physical_reserve = 1 ')
+    Item.update_all('electronic_reserve = 0', 'type = "VideoReserve" AND physical_reserve = 1 ')
+    Item.update_all('electronic_reserve = 0', 'type = "AudioReserve" AND physical_reserve = 1 ')
 
     Item.update_all('physical_reserve = 1, electronic_reserve = 1', 'type = "VideoReserve" AND physical_reserve != 1 ')
     Item.update_all('physical_reserve = 1, electronic_reserve = 1', 'type = "AudioReserve" AND physical_reserve != 1 ')
