f311616b54a1c8b4dafd564c36ff62e29763b88c
Jonathan Hartzler <Jonathan.Hartzler.3@nd.edu>
2013-12-17 12:01:16 -0500

    missed schema

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20131217151531) do
+ActiveRecord::Schema.define(version: 20131217165708) do
 
   create_table "assignments", force: true do |t|
     t.integer  "user_id"
