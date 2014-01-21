f8d8f38d4d08855e8d4675501d2f9bb60ec747fe
Jonathan Hartzler <Jonathan.Hartzler.3@nd.edu>
2014-01-21 15:14:01 -0500

    changes to the aleph importer to break it up better and to add library and processing for personal items.

---
+++ app/models/aleph_importer/determine_reserve_type.rb
@@ -0,0 +1,52 @@
+class AlephImporter::DetermineReserveType
+
+
+  def initialize(api_data)
+    @api_data = api_data
+  end
+
+
+  def determine_type
+    format_to_type[format]
+  end
+
+
+  def determine_electronic_reserve(current_reserve)
+    if current_reserve.electronic_reserve.nil?
+      ['VideoReserve', 'AudioReserve'].include?(determine_type)
+    else
+      current_reserve.electronic_reserve
+    end
+  end
+
+
+  def determine_physical_reserve(reserve)
+    if reserve.physical_reserve.nil?
+      true
+    else
+      reserve.physical_reserve
+    end
+  end
+
+
+  def format
+    @api_data['format'].downcase
+  end
+
+
+  private
+
+
+    def format_to_type
+    {
+      'book' => 'BookReserve',
+      'bound serial' => 'BookReserve',
+      'serial (unbound issue)' => 'BookReserve',
+      'dvd (visual)' => 'VideoReserve',
+      'video cassette (visual)' => 'VideoReserve',
+      'digital form (software, etc.)' => 'VideoReserve'
+    }
+  end
+
+
+end
---
+++ spec/models/aleph_importer/determine_reserve_type_spec.rb
@@ -0,0 +1,113 @@
+
+
+describe AlephImporter::DetermineReserveType do
+
+  before(:each) do
+    @api_data = { 'format' => 'Book' }
+    @api = AlephImporter::DetermineReserveType.new(@api_data)
+  end
+
+  describe :format do
+
+    it "returns the format from the api data" do
+      expect(@api.format).to eq("book")
+    end
+
+
+    it "downcases the format" do
+      expect(@api_data['format']).to receive('downcase')
+      @api.format
+    end
+  end
+
+
+  describe :determine_type do
+
+    it "determines BookReserve from Book " do
+      expect(@api.determine_type).to eq("BookReserve")
+    end
+
+
+    it "determines BookReserve from 'Bound Serial' " do
+      @api_data['format'] = 'Bound Serial'
+      expect(@api.determine_type).to eq("BookReserve")
+    end
+
+
+    it "determines BookReserve from serial (unbound issue)" do
+      @api_data['format'] = 'Serial (unbound issue)'
+      expect(@api.determine_type).to eq("BookReserve")
+    end
+
+
+    it "determines VideoReserve from DVD (visual) " do
+      @api_data['format'] = 'DVD (visual)'
+      expect(@api.determine_type).to eq("VideoReserve")
+    end
+
+
+    it "determines VideoReserve from DVD (visual) " do
+      @api_data['format'] = 'Video cassette (visual)'
+      expect(@api.determine_type).to eq("VideoReserve")
+    end
+
+
+    it "determines VideoReserve from Digital Form (software, etc.) " do
+      @api_data['format'] = 'Digital Form (software, etc.)'
+      expect(@api.determine_type).to eq("VideoReserve")
+    end
+
+
+
+    it "returns nil if the format is not found" do
+      @api_data['format'] = 'something'
+      expect(@api.determine_type).to be(nil)
+    end
+  end
+
+
+  describe :determine_electronic_reserve do
+    context :nil do
+      before(:each) do
+        @reserve = double(Reserve, electronic_reserve: nil)
+      end
+
+
+      it "returns true if the electronic_reserve has not been set and the type is VideoReserve" do
+        @api.stub(:determine_type).and_return('VideoReserve')
+        expect(@api.determine_electronic_reserve(@reserve)).to be(true)
+      end
+
+      it "returns true if the electronic_reserve has not been set and the type is AudioReserve" do
+        @api.stub(:determine_type).and_return('VideoReserve')
+        expect(@api.determine_electronic_reserve(@reserve)).to be(true)
+      end
+
+      it "returns true if the electronic_reserve has not been set and the type is BookReserve" do
+        @api.stub(:determine_type).and_return('BookReserve')
+        expect(@api.determine_electronic_reserve(@reserve)).to be(false)
+      end
+    end
+
+    context :true do
+      before(:each) do
+        @reserve = double(Reserve, electronic_reserve: true)
+      end
+
+      it "returns true if the electronic_reserve has not been set" do
+        expect(@api.determine_electronic_reserve(@reserve)).to be(true)
+      end
+    end
+
+    context :false do
+      before(:each) do
+        @reserve = double(Reserve, electronic_reserve: false)
+      end
+
+      it "returns true if the electronic_reserve has not been set" do
+        expect(@api.determine_electronic_reserve(@reserve)).to be(false)
+      end
+    end
+
+  end
+end
