4b40aac8f5890a896234dd425213bdab7487281f
Jonathan Hartzler <Jonathan.Hartzler.3@nd.edu>
2014-02-07 12:40:11 -0500

    try to get the nginx

--- app/controllers/script_controller.rb
+++ app/controllers/script_controller.rb
@@ -7,7 +7,7 @@ class ScriptController < ApplicationController
 
     #SaveRequest.update_all
 
-    response.headers['X-Accel-Redirect'] = '/reference/gis_mapping.shtml'
+    response.headers['X-Accel-Redirect'] = '/protected_videos/reference/gis_mapping.shtml'
 
     render :nothing => true
   end
