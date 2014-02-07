b80a01a47257a8a6285f2bd519eccec5c435a4a4
Jonathan Hartzler <Jonathan.Hartzler.3@nd.edu>
2014-02-07 10:06:09 -0500

    test for the redirect

--- app/controllers/script_controller.rb
+++ app/controllers/script_controller.rb
@@ -2,9 +2,11 @@ class ScriptController < ApplicationController
 
 
   def index
-    check_admin_permission!
+    # check_admin_permission!
     #  render :text => ReserveMigrator.new.import!
 
     #SaveRequest.update_all
+
+    response.headers["X-Accel-Redirect"] = '/protected_videos'
   end
 end
