603369801e3a06acd3d0d710f385279bbe4d3e81
Jonathan Hartzler <Jonathan.Hartzler.3@nd.edu>
2014-02-10 14:02:35 -0500

    fix the pass through.

--- app/controllers/script_controller.rb
+++ app/controllers/script_controller.rb
@@ -5,9 +5,9 @@ class ScriptController < ApplicationController
     # check_admin_permission!
     #  render :text => ReserveMigrator.new.import!
 
-    #SaveRequest.update_all
+    id = params[:q]
+    response.headers['X-Accel-Redirect'] = "/protected_files/#{id}"
 
-    response.headers['X-Accel-Redirect'] = '/protected_files/reference/gis_mapping.shtml'
     render :nothing => true
   end
 end
