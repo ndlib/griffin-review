5fc60e9f65e2a3e2dec581d9b84f8fa4fbf2a6b4
Jaron Kennel <jaronkk@gmail.com>
2014-05-07 10:51:27 -0400

    Fixed some migration errors

--- db/migrate/20121210200436_add_reserves_admin_role.rb
+++ db/migrate/20121210200436_add_reserves_admin_role.rb
@@ -1,4 +1,7 @@
 class AddReservesAdminRole < ActiveRecord::Migration
+  class Role < ActiveRecord::Base
+  end
+
   def up
     Role.where(:name => 'Reserves Admin').first_or_create(:description => 'This role allows the user to administer almost all aspects of the reserves system.')
   end
