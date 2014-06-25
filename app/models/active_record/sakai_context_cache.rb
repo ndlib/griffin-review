daf60675a8d356ee6b6a3a537811c38835660674
Jonathan Hartzler <Jonathan.Hartzler.3@nd.edu>
2014-06-25 10:11:43 -0400

    fix bug where i forgot to remove the term from the validations.

--- app/models/active_record/sakai_context_cache.rb
+++ app/models/active_record/sakai_context_cache.rb
@@ -2,7 +2,7 @@ class SakaiContextCache < ActiveRecord::Base
 
   self.table_name = 'sakai_context_cache'
 
-  validates :context_id, :course_id, :term, presence: true
+  validates :context_id, :course_id, presence: true
 
   scope :by_term, lambda { | term_id | where(term: term_id) }
 
