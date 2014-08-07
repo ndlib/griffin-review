f96fb3908c9016bacc21cb11b5c3868f10b91141
Jonathan Hartzler <Jonathan.Hartzler.3@nd.edu>
2014-08-07 15:34:31 -0400

    updated the admin list to have the instructor listed not the requestor.

--- db/schema.rb
+++ db/schema.rb
@@ -16,8 +16,8 @@ ActiveRecord::Schema.define(version: 20140805171034) do
   create_table "assignments", force: true do |t|
     t.integer  "user_id"
     t.integer  "role_id"
-    t.datetime "created_at"
-    t.datetime "updated_at"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
   end
 
   create_table "error_logs", force: true do |t|
@@ -60,10 +60,10 @@ ActiveRecord::Schema.define(version: 20140805171034) do
     t.string   "selection_title"
     t.text     "description"
     t.integer  "item_type_id"
-    t.datetime "created_at"
-    t.datetime "updated_at"
+    t.datetime "created_at",                    null: false
+    t.datetime "updated_at",                    null: false
     t.string   "creator"
-    t.string   "title"
+    t.text     "title"
     t.string   "journal_title"
     t.string   "nd_meta_data_id"
     t.boolean  "overwrite_nd_meta_data"
@@ -99,8 +99,8 @@ ActiveRecord::Schema.define(version: 20140805171034) do
     t.string   "language"
     t.string   "subtitles"
     t.text     "note"
-    t.datetime "created_at"
-    t.datetime "updated_at"
+    t.datetime "created_at",                 null: false
+    t.datetime "updated_at",                 null: false
     t.string   "workflow_state"
     t.datetime "workflow_state_change_date"
     t.integer  "workflow_state_change_user"
@@ -151,8 +151,8 @@ ActiveRecord::Schema.define(version: 20140805171034) do
     t.string   "full_name"
     t.date     "date_begin"
     t.date     "date_end"
-    t.datetime "created_at"
-    t.datetime "updated_at"
+    t.datetime "created_at",      null: false
+    t.datetime "updated_at",      null: false
     t.string   "movie_directory"
   end
 
@@ -177,8 +177,8 @@ ActiveRecord::Schema.define(version: 20140805171034) do
     t.string   "current_sign_in_ip"
     t.string   "last_sign_in_ip"
     t.string   "username"
-    t.datetime "created_at"
-    t.datetime "updated_at"
+    t.datetime "created_at",                      null: false
+    t.datetime "updated_at",                      null: false
     t.boolean  "admin"
     t.string   "admin_preferences"
   end
@@ -201,11 +201,9 @@ ActiveRecord::Schema.define(version: 20140805171034) do
     t.string   "username"
     t.string   "token"
     t.string   "ip"
-    t.integer  "timestamp"
+    t.string   "timestamp"
     t.datetime "created_at"
     t.datetime "updated_at"
   end
-
-  add_index "wowza_tokens", ["token"], name: "index_wowza_tokens_on_token", using: :btree
 
 end
--- spec/models/view/request_row_spec.rb
+++ spec/models/view/request_row_spec.rb
@@ -57,6 +57,10 @@ describe RequestRow do
   end
 
 
+  it "has a instructor_col" do
+    expect(RequestRow.new(Reserve.new).respond_to?(:instructor_col)).to be_true
+  end
+
   it "has a column for course" do
     expect(RequestRow.new(Reserve.new).respond_to?(:course_col)).to be_true
   end
@@ -74,11 +78,11 @@ describe RequestRow do
 
 
   it "makes an array for json rendering" do
-    Reserve.any_instance.stub(:course).and_return(double(Course, id: 'course_id', semester: FactoryGirl.create(:semester), full_title: 'Course'))
+    Reserve.any_instance.stub(:course).and_return(double(Course, id: 'course_id', semester: FactoryGirl.create(:semester), full_title: 'Course', primary_instructor: double(User, first_name: 'fname', last_name: 'lname', username: 'username')))
     r = Reserve.new(title: 'json', needed_by: '1/1/2013', requestor_netid: 'jhartzle', course_id: 'course_id', type: 'VideoReserve', physical_reserve: true, electronic_reserve: false)
     r.save!
 
-    expect(RequestRow.new(r).to_json).to eq([" 1 Jan", "<a href=\"/admin/requests/2\" target=\"_blank\">json</a>", Time.now.to_date.to_s(:short), "<a href=\"/masquerades/new?username=jhartzle\"></a>", "<a href=\"/courses/course_id/reserves\" target=\"_blank\">Course</a>", "Video", r.created_at.to_time.to_i, 1357016400, " physical"])
+    expect(RequestRow.new(r).to_json).to eq([" 1 Jan", "<a href=\"/admin/requests/2\" target=\"_blank\">json</a>", " 7 Aug", "<a href=\"/masquerades/new?username=username\">lname, fname</a>", "<a href=\"/courses/course_id/reserves\" target=\"_blank\">Course</a>", "Video", r.created_at.to_time.to_i, 1357016400, " physical"])
   end
 
 
