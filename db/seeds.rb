a16db4311ed6b67f85715052eb414b0840defea6
Jaron Kennel <jaronkk@gmail.com>
2014-08-14 11:46:48 -0400

    Updated seeds task to create better semesters

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,7 +5,16 @@
 #
 #   cities = City.create([{ :name => 'Chicago' }, { :name => 'Copenhagen' }])
 #   Mayor.create(:name => 'Emanuel', :city => cities.first)
+def create_terms(year)
+  summer_begin = Date.parse("#{year}-05-16")
+  summer_end = Date.parse("#{year}-08-05")
+  fall_end = summer_begin.end_of_year
+  spring_end = Date.parse("#{year+1}-05-15")
+  Semester.create!( :full_name => "Summer #{year}", :code => "#{year}00", :date_begin => summer_begin, :date_end => summer_end)
+  Semester.create!( :full_name => "Fall #{year}", :code => "#{year}10", :date_begin => 1.day.since(summer_end), :date_end => fall_end)
+  Semester.create!( :full_name => "Spring #{year}", :code => "#{year}20", :date_begin => 1.day.since(fall_end), :date_end => spring_end)
+end
 
-#Semester.create!( :full_name => 'Summer 2013', :code => '201300', :date_begin => 2.months.ago, :date_end => 1.month.from_now)
-Semester.create!( :full_name => 'Summer 2013', :code => '201220', :date_begin => 5.months.ago, :date_end => 1.month.ago)
-Semester.create!( :full_name => 'Fall 2013', :code => '201310', :date_begin => 1.month.ago, :date_end => 5.months.from_now)
+Semester.destroy_all
+create_terms(Date.today.year - 1)
+create_terms(Date.today.year)
