8d1106b6d7d769770c9413210b57a7d68d10e1c1
Jonathan Hartzler <Jonathan.Hartzler.3@nd.edu>
2014-08-14 11:12:45 -0400

    update schema file to be correct.

--- db/schema.rb
+++ db/schema.rb
@@ -16,8 +16,8 @@ ActiveRecord::Schema.define(version: 20140721154328) do
   create_table "assignments", force: true do |t|
     t.integer  "user_id"
     t.integer  "role_id"
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
+    t.datetime "created_at"
+    t.datetime "updated_at"
   end
 
   create_table "error_logs", force: true do |t|
@@ -60,8 +60,8 @@ ActiveRecord::Schema.define(version: 20140721154328) do
     t.string   "selection_title"
     t.text     "description"
     t.integer  "item_type_id"
-    t.datetime "created_at",                    null: false
-    t.datetime "updated_at",                    null: false
+    t.datetime "created_at"
+    t.datetime "updated_at"
     t.string   "creator"
     t.text     "title"
     t.string   "journal_title"
@@ -98,8 +98,8 @@ ActiveRecord::Schema.define(version: 20140721154328) do
     t.string   "language"
     t.string   "subtitles"
     t.text     "note"
-    t.datetime "created_at",                 null: false
-    t.datetime "updated_at",                 null: false
+    t.datetime "created_at"
+    t.datetime "updated_at"
     t.string   "workflow_state"
     t.datetime "workflow_state_change_date"
     t.integer  "workflow_state_change_user"
@@ -150,8 +150,8 @@ ActiveRecord::Schema.define(version: 20140721154328) do
     t.string   "full_name"
     t.date     "date_begin"
     t.date     "date_end"
-    t.datetime "created_at",      null: false
-    t.datetime "updated_at",      null: false
+    t.datetime "created_at"
+    t.datetime "updated_at"
     t.string   "movie_directory"
   end
 
@@ -176,8 +176,8 @@ ActiveRecord::Schema.define(version: 20140721154328) do
     t.string   "current_sign_in_ip"
     t.string   "last_sign_in_ip"
     t.string   "username"
-    t.datetime "created_at",                      null: false
-    t.datetime "updated_at",                      null: false
+    t.datetime "created_at"
+    t.datetime "updated_at"
     t.boolean  "admin"
     t.string   "admin_preferences"
   end
@@ -200,9 +200,11 @@ ActiveRecord::Schema.define(version: 20140721154328) do
     t.string   "username"
     t.string   "token"
     t.string   "ip"
-    t.string   "timestamp"
+    t.integer  "timestamp"
     t.datetime "created_at"
     t.datetime "updated_at"
   end
+
+  add_index "wowza_tokens", ["token"], name: "index_wowza_tokens_on_token", using: :btree
 
 end
