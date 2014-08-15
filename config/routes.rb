67986ef110e98916f42707fdeb967746659be023
Robert Fox <robertjonfox@gmail.com>
2014-08-15 11:23:27 -0400

    Add route for changing the needed by date

--- config/routes.rb
+++ config/routes.rb
@@ -61,6 +61,7 @@ Griffin::Application.routes.draw do
     resources :resources, controller: 'requests_resources'
     resources :types, controller: 'requests_type', only: ['update', 'edit']
     resources :libraries, controller: 'requests_library', only: ['update']
+    resources :needed_by, controller: 'requests_needed_by', only: ['update']
 
     resources :semesters, controller: 'semesters'
     get 'semesters/csv/:semester_id', controller: 'semesters', action: :csv_report, as: 'csv_report'
