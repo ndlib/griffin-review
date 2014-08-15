9f8c577e6d73100d862c2bb741dc6e09556f9a06
Robert Fox <robertjonfox@gmail.com>
2014-08-15 11:23:45 -0400

    Add tests for needed by chage date form model

---
+++ spec/models/forms/needed_by_reserve_form_spec.rb
@@ -0,0 +1,40 @@
+
+describe NeededByReserveForm do
+
+  before(:each) do
+    @course = double(Course, id: 'id')
+
+    @reserve = double(Reserve, id: 'id', needed_by: '2051-01-15')
+    @reserve.stub(:save!).and_return(true)
+  end
+
+  describe :initialize do
+
+    it "sets reserve attributes to the virtus attributes" do
+      @form = NeededByReserveForm.new(@reserve, {})
+      expect(@form.send(:needed_by)).to be_true
+    end
+
+
+    it "overwrites the value with what is in params" do
+      params = { needed_by: '2045-01-20' }
+      @form = NeededByReserveForm.new(@reserve, params)
+      expect(@form.send(:needed_by)).to eq '2045-01-20'
+    end
+
+  end
+
+  describe :persistance do
+    before(:each) do
+      @course = double(Course, id: 'id', semester: FactoryGirl.create(:semester))
+      @reserve = Reserve.new(id: 'id', title: 'title', course: @course, type: 'BookChapterReserve', requestor_netid: 'netid', physical_reserve: false, electronic_reserve: false, library: 'hesburgh')
+    end
+
+    it "saves the needed by date" do
+      @form = NeededByReserveForm.new(@reserve, {needed_by: '2031-03-14'})
+      @form.update_reserve_needed_by!
+      expect(@form.reserve.needed_by).to eq 'Fri, 14 Mar 2031'.to_date
+    end
+
+  end
+end
