b8b73daa048780574191770f6f2a18aa7afd2975
Jaron Kennel <jaronkk@gmail.com>
2014-08-20 09:55:38 -0400

    Update them cached item fields on the requests

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20140819192204) do
+ActiveRecord::Schema.define(version: 20140820134818) do
 
   create_table "assignments", force: true do |t|
     t.integer  "user_id"
