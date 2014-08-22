faacac485b07bae737aa7fb778b3a0003d78a74f
Jaron Kennel <jaronkk@gmail.com>
2014-08-22 16:23:30 -0400

    More RequestFilter cleanup

--- app/models/view/request_filter.rb
+++ app/models/view/request_filter.rb
@@ -21,18 +21,18 @@ class RequestFilter
 
 
   def library_selected?(library)
-    @library_filters.include?(library)
+    library_filters.include?(library)
   end
 
 
   def type_selected?(type)
-    @type_filters.include?(type)
+    type_filters.include?(type)
   end
 
 
   def save_filter_for_user!
-    user.libraries = @library_filters
-    user.types = @type_filters
+    user.libraries = library_filters
+    user.types = type_filters
     user.save!
   end
 
@@ -41,19 +41,19 @@ class RequestFilter
 
     def determine_filters
       if session[:admin_request_filter]
-        @library_filters = session[:admin_request_filter][:libraries]
-        @type_filters    = session[:admin_request_filter][:types]
-        @semester_filter = session[:admin_request_filter][:semester]
+        self.library_filters = session[:admin_request_filter][:libraries]
+        self.type_filters    = session[:admin_request_filter][:types]
+        self.semester_filter = session[:admin_request_filter][:semester]
 
       elsif user.admin_preferences.present?
-        @library_filters = user.libraries
-        @type_filters    = user.types
-        @semester_filter = false
+        self.library_filters = user.libraries
+        self.type_filters    = user.types
+        self.semester_filter = false
 
       else
-        @library_filters = VALID_LIBRARIES
-        @type_filters    = VALID_TYPES
-        @semester_filter = false
+        self.library_filters = VALID_LIBRARIES
+        self.type_filters    = VALID_TYPES
+        self.semester_filter = false
 
       end
     end
@@ -62,16 +62,16 @@ class RequestFilter
     def process_params
       if params[:admin_request_filter]
         if params[:admin_request_filter][:libraries]
-          @library_filters = params[:admin_request_filter][:libraries]
+          self.library_filters = params[:admin_request_filter][:libraries]
         end
         if params[:admin_request_filter][:types]
-          @type_filters = params[:admin_request_filter][:types]
+          self.type_filters = params[:admin_request_filter][:types]
         end
         if params[:admin_request_filter][:semester_id]
           if params[:admin_request_filter][:semester_id] != 'false'
-            @semester_filter = params[:admin_request_filter][:semester_id]
+            self.semester_filter = params[:admin_request_filter][:semester_id]
           else
-            @semester_filter = false
+            self.semester_filter = false
           end
         end
 
@@ -83,9 +83,9 @@ class RequestFilter
 
     def save_in_session!
       session[:admin_request_filter] ||= {}
-      session[:admin_request_filter][:libraries] = @library_filters
-      session[:admin_request_filter][:types] = @type_filters
-      session[:admin_request_filter][:semester] = @semester_filter
+      session[:admin_request_filter][:libraries] = library_filters
+      session[:admin_request_filter][:types] = type_filters
+      session[:admin_request_filter][:semester] = semester_filter
 
     end
 end
