1786c73c93af8abc53de639f852abfecf96c0810
Jaron Kennel <jaronkk@gmail.com>
2014-08-25 08:58:44 -0400

    Cleaned up request filter spec

--- spec/models/view/request_filter_spec.rb
+++ spec/models/view/request_filter_spec.rb
@@ -2,33 +2,35 @@ require 'spec_helper'
 
 
 describe RequestFilter do
-
-
-  before(:each) do
-    @controller = ApplicationController.new
-    @controller.stub(:current_user).and_return(double(User, id: 1, username: 'usernaem', admin_preferences: {}))
-    @controller.stub(:session).and_return({ } )
-    @controller.stub(:params).and_return( { } )
+  let(:user) { double(User, id: 1, username: 'username', admin_preferences: {}) }
+  let(:session) { {} }
+  let(:params) { {} }
+  let(:controller) do
+    ApplicationController.new.tap do |c|
+      c.stub(:current_user).and_return(user)
+      c.stub(:session).and_return( session )
+      c.stub(:params).and_return( params )
+    end
   end
+
+  subject { described_class.new(controller)}
 
   describe :session_saving do
 
     it "saves the status value in the session when it is changed " do
-      @controller.params[:admin_request_filter] = { libraries: [ 'library1', 'library2'], types: [ 'types' ], semester_id: 1  }
-      arf = RequestFilter.new(@controller)
+      params[:admin_request_filter] = { libraries: [ 'library1', 'library2'], types: [ 'types' ], semester_id: 1  }
 
-      expect(arf.library_filters).to eq([ 'library1', 'library2'])
-      expect(arf.type_filters).to eq([ 'types' ])
-      expect(arf.semester_filter).to eq(1)
+      expect(subject.library_filters).to eq([ 'library1', 'library2'])
+      expect(subject.type_filters).to eq([ 'types' ])
+      expect(subject.semester_filter).to eq(1)
     end
 
 
     it "saves the new params over the old session data " do
-      @controller.session[:admin_request_filter] = { libraries: [ 'library1', 'library2'], types: [ 'types' ] }
-      @controller.params[:admin_request_filter] = { libraries: [ 'library1'], types: [ 'type1' ], semester_id: 1  }
-      arf = RequestFilter.new(@controller)
+      session[:admin_request_filter] = { libraries: [ 'library1', 'library2'], types: [ 'types' ] }
+      params[:admin_request_filter] = { libraries: [ 'library1'], types: [ 'type1' ], semester_id: 1  }
 
-      expect(@controller.session).to eq( { admin_request_filter: { libraries: [ 'library1'], types: [ 'type1' ], semester: 1 } } )
+      expect(subject.session).to eq( { admin_request_filter: { libraries: [ 'library1'], types: [ 'type1' ], semester: 1 } } )
     end
   end
 
@@ -36,44 +38,39 @@ describe RequestFilter do
   describe :determine_filters do
 
     it "defaults to everything if there is nothing to choose from" do
-      arf = RequestFilter.new(@controller)
-      expect(arf.library_filters).to eq(RequestFilter::VALID_LIBRARIES)
-      expect(arf.type_filters).to eq(RequestFilter::VALID_TYPES)
-      expect(arf.semester_filter).to be(false)
+      expect(subject.library_filters).to eq(described_class::VALID_LIBRARIES)
+      expect(subject.type_filters).to eq(described_class::VALID_TYPES)
+      expect(subject.semester_filter).to be(false)
     end
 
 
     it "reloads from the session " do
-      @controller.session[:admin_request_filter] = { libraries: [ 'library1', 'library2'], types: [ 'types' ], semester: 1  }
+      session[:admin_request_filter] = { libraries: [ 'library1', 'library2'], types: [ 'types' ], semester: 1  }
 
-      arf = RequestFilter.new(@controller)
-
-      expect(arf.library_filters).to eq([ 'library1', 'library2'])
-      expect(arf.type_filters).to eq([ 'types' ])
-      expect(arf.semester_filter).to eq(1)
+      expect(subject.library_filters).to eq([ 'library1', 'library2'])
+      expect(subject.type_filters).to eq([ 'types' ])
+      expect(subject.semester_filter).to eq(1)
     end
 
+    describe 'user with preferences' do
+      before do
+        user.stub(:admin_preferences).and_return({:libraries => '32432423'})
+        user.stub(:libraries).and_return([ 'library1', 'library2'])
+        user.stub(:types).and_return( [ 'types' ] )
+      end
 
-    it "loads the filters from the user record" do
-      @controller.current_user.stub(:admin_preferences).and_return({:libraries => '32432423'})
-      @controller.current_user.stub(:libraries).and_return([ 'library1', 'library2'])
-      @controller.current_user.stub(:types).and_return( [ 'types' ] )
+      it "loads the filters from the user record" do
 
-      arf = RequestFilter.new(@controller)
-
-      expect(arf.library_filters).to eq([ 'library1', 'library2'])
-      expect(arf.type_filters).to eq([ 'types' ])
-    end
+        expect(subject.library_filters).to eq([ 'library1', 'library2'])
+        expect(subject.type_filters).to eq([ 'types' ])
+      end
 
 
-    it "does not load the semester from the user record" do
-      @controller.current_user.stub(:admin_preferences).and_return({:libraries => '32432423'})
-      @controller.current_user.stub(:libraries).and_return([ 'library1', 'library2'])
-      @controller.current_user.stub(:types).and_return( [ 'types' ] )
-      @controller.current_user.stub(:semeseter).and_return( 1 )
+      it "does not load the semester from the user record" do
+        user.stub(:semeseter).and_return( 1 )
 
-      arf = RequestFilter.new(@controller)
-      expect(arf.semester_filter).to be(false)
+        expect(subject.semester_filter).to be(false)
+      end
     end
   end
 
@@ -81,18 +78,16 @@ describe RequestFilter do
   describe :library_selected? do
 
     it "returns true if the library selected is in the filters for the library" do
-      @controller.session[:admin_request_filter] = { libraries: [ 'library1', 'library2'], types: [ 'types' ] }
+      session[:admin_request_filter] = { libraries: [ 'library1', 'library2'], types: [ 'types' ] }
 
-      arf = RequestFilter.new(@controller)
-      expect(arf.library_selected?('library1')).to be_true
+      expect(subject.library_selected?('library1')).to be_true
     end
 
 
     it "returns fakse if the library selected is in the filters for the library" do
-      @controller.session[:admin_request_filter] = { libraries: [ 'library1', 'library2'], types: [ 'types' ] }
+      session[:admin_request_filter] = { libraries: [ 'library1', 'library2'], types: [ 'types' ] }
 
-      arf = RequestFilter.new(@controller)
-      expect(arf.library_selected?('library3')).to be_false
+      expect(subject.library_selected?('library3')).to be_false
     end
   end
 
@@ -100,18 +95,16 @@ describe RequestFilter do
   describe :type_selected? do
 
     it "returns true if the type selected is in the filters for the type" do
-      @controller.session[:admin_request_filter] = { libraries: [ 'library1', 'library2'], types: [ 'types' ] }
+      session[:admin_request_filter] = { libraries: [ 'library1', 'library2'], types: [ 'types' ] }
 
-      arf = RequestFilter.new(@controller)
-      expect(arf.type_selected?('types')).to be_true
+      expect(subject.type_selected?('types')).to be_true
     end
 
 
     it "returns fakse if the type selected is in the filters for the types" do
-      @controller.session[:admin_request_filter] = { libraries: [ 'library1', 'library2'], types: [ 'types' ] }
+      session[:admin_request_filter] = { libraries: [ 'library1', 'library2'], types: [ 'types' ] }
 
-      arf = RequestFilter.new(@controller)
-      expect(arf.type_selected?('other type')).to be_false
+      expect(subject.type_selected?('other type')).to be_false
     end
   end
 
