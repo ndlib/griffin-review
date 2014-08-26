e0fe8298ef039550f449038d2a1c32a91d71e50a
Jonathan Hartzler <Jonathan.Hartzler.3@nd.edu>
2014-08-26 09:04:00 -0400

    added a flash message to en.yml for success and errors.

--- app/controllers/requests_fix_missing_courses_controller.rb
+++ app/controllers/requests_fix_missing_courses_controller.rb
@@ -11,8 +11,10 @@ class RequestsFixMissingCoursesController  < ApplicationController
     @request = RequestFixMissingCourseForm.build_from_params(self)
 
     if @request.update_course_id!
+      flash[:success] = t("successes.messages.fix_missing_courses", old_course_id: @request.old_course_id, new_course_id: @request.new_course_id)
       redirect_to request_path(@request.id)
     else
+      flash.now[:error] = t('errors.messages.form_error')
       render :edit
     end
   end
