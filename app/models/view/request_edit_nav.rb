8c2330e4c6ec6e0e1bca743b7c590072007a9b1e
Jonathan Hartzler <Jonathan.Hartzler.3@nd.edu>
2014-08-26 10:13:08 -0400

    update the request edit nav to use the policy for testing if a course is missing

--- app/models/view/request_edit_nav.rb
+++ app/models/view/request_edit_nav.rb
@@ -1,5 +1,5 @@
 class RequestEditNav
-
+  attr_reader :reserve
   include RailsHelpers
 
   def initialize(reserve)
@@ -8,25 +8,25 @@ class RequestEditNav
 
 
   def reserve_id
-    @reserve.id
+    reserve.id
   end
 
 
   def removed?
-    @reserve.workflow_state == 'removed'
+    reserve.workflow_state == 'removed'
   end
 
 
   def on_order_form
-    PlaceItemOnOrderForm.new(@reserve)
+    PlaceItemOnOrderForm.new(reserve)
   end
 
 
   def meta_data_notes
     uls = []
-    if @reserve.nd_meta_data_id.present?
-      uls << "Record ID: #{@reserve.nd_meta_data_id}"
-      uls << "Syncronized on #{@reserve.metadata_synchronization_date.to_s(:long)}"
+    if reserve.nd_meta_data_id.present?
+      uls << "Record ID: #{reserve.nd_meta_data_id}"
+      uls << "Syncronized on #{reserve.metadata_synchronization_date.to_s(:long)}"
 
     elsif meta_data_policy.meta_data_id_required? && !meta_data_policy.complete?
       uls << "Requires a Record ID"
@@ -50,7 +50,7 @@ class RequestEditNav
     uls = []
     if requires_electronic_resource?
       if electronic_resouce_comeplete?
-        erpolicy = ElectronicReservePolicy.new(@reserve)
+        erpolicy = ElectronicReservePolicy.new(reserve)
         uls << "#{erpolicy.electronic_resource_type}: #{erpolicy.resource_name.truncate(40)}"
       else
         uls << "Requires an Electronic Resource"
@@ -65,7 +65,7 @@ class RequestEditNav
     uls = []
     if requires_fair_use?
       if fair_use_complete?
-        uls << @reserve.fair_use.state.titleize
+        uls << reserve.fair_use.state.titleize
       else
         uls << "Requires Form Completion"
       end
@@ -76,48 +76,53 @@ class RequestEditNav
 
 
   def complete?
-    ReserveCheckIsComplete.new(@reserve).complete?
+    ReserveCheckIsComplete.new(reserve).complete?
   end
 
 
   def meta_data_complete?
-    ReserveMetaDataPolicy.new(@reserve).complete?
+    ReserveMetaDataPolicy.new(reserve).complete?
   end
 
 
   def requires_electronic_resource?
-    ElectronicReservePolicy.new(@reserve).can_have_resource?
+    ElectronicReservePolicy.new(reserve).can_have_resource?
   end
 
 
   def electronic_resouce_comeplete?
-    ElectronicReservePolicy.new(@reserve).complete?
+    ElectronicReservePolicy.new(reserve).complete?
   end
 
 
   def requires_fair_use?
-    ReserveFairUsePolicy.new(@reserve).requires_fair_use?
+    ReserveFairUsePolicy.new(reserve).requires_fair_use?
   end
 
 
   def fair_use_complete?
-    ReserveFairUsePolicy.new(@reserve).complete?
+    ReserveFairUsePolicy.new(reserve).complete?
   end
 
 
   def physical_reserve?
-    PhysicalReservePolicy.new(@reserve).is_physical_reserve?
+    PhysicalReservePolicy.new(reserve).is_physical_reserve?
   end
 
 
   def reserve_in_aleph?
-    ReserveInAlephPolicy.new(@reserve).in_aleph?
+    ReserveInAlephPolicy.new(reserve).in_aleph?
+  end
+
+
+  def show_fix_course_id?
+    ReserveIsMissingCoursePolicy.call(reserve)
   end
 
   private
 
     def meta_data_policy
-      @policy ||= ReserveMetaDataPolicy.new(@reserve)
+      @policy ||= ReserveMetaDataPolicy.new(reserve)
     end
 
 
--- spec/models/view/request_edit_form_spec.rb
+++ spec/models/view/request_edit_form_spec.rb
@@ -1,25 +1,24 @@
 
 describe RequestEditNav do
 
-  before(:each) do
-    @reserve = double(Reserve, id: 1, course: double(Course, id: 1))
-  end
+  let(:reserve) { double(Reserve, id: 1, course: double(Course, id: 1)) }
 
   it "passes the reseve id out " do
-    aeb = RequestEditNav.new(@reserve)
+    aeb = RequestEditNav.new(reserve)
     aeb.reserve_id.should == 1
   end
 
   describe :meta_data_complete? do
+
     it "returns true if the meta data is complete" do
       ReserveMetaDataPolicy.any_instance.stub(:complete?).and_return(true)
-      RequestEditNav.new(@reserve).meta_data_complete?.should be_true
+      RequestEditNav.new(reserve).meta_data_complete?.should be_true
     end
 
 
     it "returns false if the meta data is not complete" do
       ReserveMetaDataPolicy.any_instance.stub(:complete?).and_return(false)
-      RequestEditNav.new(@reserve).meta_data_complete?.should be_false
+      RequestEditNav.new(reserve).meta_data_complete?.should be_false
     end
   end
 
@@ -29,15 +28,30 @@ describe RequestEditNav do
 
     it "returns true if all the sub parts are true" do
       ReserveCheckIsComplete.any_instance.stub(:complete?).and_return(true)
-      RequestEditNav.new(@reserve).complete?.should be_true
+      RequestEditNav.new(reserve).complete?.should be_true
     end
 
 
     it "returns false if one of the sub parts are not true" do
       ReserveCheckIsComplete.any_instance.stub(:complete?).and_return(false)
-      RequestEditNav.new(@reserve).complete?.should be_false
+      RequestEditNav.new(reserve).complete?.should be_false
     end
   end
 
+
+
+  describe :show_fix_course_id? do
+
+    it "returns true if the coures is the missing course" do
+      ReserveIsMissingCoursePolicy.stub(:call).and_return(true)
+      expect(RequestEditNav.new(reserve).show_fix_course_id?).to be_true
+    end
+
+
+    it "returns false if the coures is the missing course" do
+      ReserveIsMissingCoursePolicy.stub(:call).and_return(false)
+      expect(RequestEditNav.new(reserve).show_fix_course_id?).to be_false
+    end
+  end
 end
 
