9f5b03f617ce8607bc4618f6dae77b34ab2f2cfc
Jaron Kennel <jaronkk@gmail.com>
2014-08-26 13:45:30 -0400

    Store additional admin preferences on the user model

--- spec/models/active_record/user_spec.rb
+++ spec/models/active_record/user_spec.rb
@@ -1,15 +1,27 @@
 require 'spec_helper'
 
 describe User do
-
+  subject { described_class.new }
 
   it "downcases the username no matter what was entered" do
-    expect(User.new(username: "UPPER").username).to eq("upper")
+    subject.username = "UPPER"
+    expect(subject.username).to eq("upper")
   end
 
 
   it "strips the username no matter what was entered" do
-    expect(User.new(username: " strip ").username).to eq("strip")
+    subject.username = " strip"
+    expect(subject.username).to eq("strip")
+  end
+
+  describe 'admin_preferences' do
+    [:libraries, :types, :reserve_type, :instructor_range_begin, :instructor_range_end].each do |method|
+      it "stores #{method}" do
+        expect(subject).to respond_to(method)
+        subject.send("#{method}=", method.to_s)
+        expect(subject.send(method)).to eq(method.to_s)
+      end
+    end
   end
 
 end
--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -38,6 +38,11 @@ Spork.prefork do
 
   # This file is copied to spec/ when you run 'rails generate rspec:install'
   ENV["RAILS_ENV"] ||= 'test'
+
+  # Trap methods to prevent model caching: https://github.com/sporkrb/spork/wiki/Spork.trap_method-Jujitsu
+  require "rails/application"
+  Spork.trap_method(Rails::Application::RoutesReloader, :reload!)
+
   require File.expand_path("../../config/environment", __FILE__)
   require 'rspec/rails'
   require 'rspec/autorun'
