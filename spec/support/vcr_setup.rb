156dccbabf512f4f3d89e5c3b5bf711e3b515dc5
Robert Fox <robertjonfox@gmail.com>
2014-08-28 13:57:27 -0400

    Tell VCR to ignore connections to calendar.nd.edu

--- spec/support/vcr_setup.rb
+++ spec/support/vcr_setup.rb
@@ -17,6 +17,9 @@ module VCRSetup
       c.cassette_library_dir = 'spec/fixtures/vcr_cassettes'
       c.hook_into :webmock
       c.allow_http_connections_when_no_cassette = false
+      c.ignore_hosts (
+        'calendar.nd.edu'
+      )
     end
   end
 end
