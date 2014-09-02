77a4d55674982f1c0df3ae773f1bfa5d828db0b1
Jonathan Hartzler <Jonathan.Hartzler.3@nd.edu>
2014-09-02 14:35:44 -0400

    add the table for the playlists

---
+++ db/migrate/20140902142421_create_media_playlists.rb
@@ -0,0 +1,9 @@
+class CreateMediaPlaylists < ActiveRecord::Migration
+  def change
+    create_table :media_playlists do |t|
+      t.integer :item_id
+      t.text :data, :limit => 4294967295
+    end
+
+  end
+end
