1499056f08ea54504ee5a46f82c35ae7ff9dc4e4
Jonathan Hartzler <Jonathan.Hartzler.3@nd.edu>
2014-09-03 15:54:53 -0400

    add media playlist spec

--- app/models/active_record/media_playlist.rb
+++ app/models/active_record/media_playlist.rb
@@ -1,5 +1,7 @@
 class MediaPlaylist < ActiveRecord::Base
 
+  validates :item_id, presence: true
+
   store :data, accessors: [:rows]
 
 end
---
+++ spec/models/active_record/media_playlist_spec.rb
@@ -0,0 +1,20 @@
+require 'spec_helper'
+
+describe MediaPlaylist do
+
+  it "has item_id" do
+    expect(subject).to respond_to(:item_id)
+    expect(subject).to respond_to(:item_id=)
+  end
+
+  it "has rows" do
+    expect(subject).to respond_to(:rows)
+    expect(subject).to respond_to(:rows=)
+  end
+
+  it " requires a item_id" do
+    subject.should have(1).error_on(:item_id)
+  end
+
+
+end
