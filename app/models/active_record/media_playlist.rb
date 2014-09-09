b1008e55665d8b190c002d2e117bed49a8729856
Jonathan Hartzler <Jonathan.Hartzler.3@nd.edu>
2014-09-09 08:27:21 -0400

    changed the validations on type

--- app/models/active_record/media_playlist.rb
+++ app/models/active_record/media_playlist.rb
@@ -1,6 +1,7 @@
 class MediaPlaylist < ActiveRecord::Base
 
-  validates :item_id, presence: true
+  validates :item_id, :type, presence: true
+  validates :type, inclusion: { in: %w(audio, video) }
 
   store :data, accessors: [:rows]
 
