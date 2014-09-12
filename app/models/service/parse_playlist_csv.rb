b70ee55bfb5cb4e984a7eacd2f5ee16a7f86a038
Jonathan Hartzler <Jonathan.Hartzler.3@nd.edu>
2014-09-12 12:00:11 -0400

    added csv upload.

--- app/models/service/parse_playlist_csv.rb
+++ app/models/service/parse_playlist_csv.rb
@@ -12,23 +12,38 @@ class ParsePlaylistCsv
   end
 
   def rows
-    playlist_rows.collect { | row | translate_row(row) }
+    out = []
+    playlist_rows.each_with_index do |row, line|
+      if line == 0 || match_extra_row?(row)
+        next
+      end
+
+      out << translate_row(row);
+    end
+
+    out
   end
 
   private
 
+    def match_extra_row?(row)
+      row.match(/build on .* with Mp3tag v2.50 - the universal Tag editor - http:\/\/www.mp3tag.de\/en\//)
+    end
+
     def playlist_rows
-      CSV.foreach(file, { col_sep: ';', headers: :first_row } )
+      File.open(file.path, 'r', encoding: 'bom|utf-8').each_line
     end
 
     def translate_row(row)
+      row = row.split(';')
       {
         'title' => row[0],
-        'filename' => parse_directory(row[8]) + row[9]
+        'filename' => parse_directory(row[row.size - 3]) + row[row.size - 2]
       }
     end
 
     def parse_directory(directory)
       directory.gsub(/\\/,'/').gsub('L:/Departmental/Digital Library Services/Private/StreamingAudio/', '')
     end
+
 end
--- spec/models/service/parse_playlist_csv_spec.rb
+++ spec/models/service/parse_playlist_csv_spec.rb
@@ -2,29 +2,29 @@ require 'spec_helper'
 
 
 describe ParsePlaylistCsv do
-
+  subject { described_class }
   let(:upload_file) { fixture_file_upload("#{Rails.root}/spec/fixtures/mp3tag.csv", 'application/csv') }
 
   it "takes a mp3tag title and adds it to the playlist" do
-    expect(ParsePlaylistCsv.rows(upload_file).first['title']).to eq("Brahms: String Quartet No.1 in C minor, Op.51 No.1: I. Allegro")
+    expect(subject.rows(upload_file).first['title']).to eq("Brahms: String Quartet No.1 in C minor, Op.51 No.1: I. Allegro")
   end
 
   it "takes a mp3tag  file and adds it to the playlist" do
-    expect(ParsePlaylistCsv.rows(upload_file).first['filename']).to match("C01528-11_1.mp3")
+    expect(subject.rows(upload_file).first['filename']).to match("C01528-11_1.mp3")
   end
 
   it "uses parse_directory" do
-    expect_any_instance_of(ParsePlaylistCsv).to receive(:parse_directory).and_return("")
-    ParsePlaylistCsv.rows(upload_file)
+    expect_any_instance_of(subject).to receive(:parse_directory).at_least(:once).and_return("")
+    subject.rows(upload_file)
   end
 
   it "converts the \ in the path to a / " do
-    csv = ParsePlaylistCsv.new(upload_file)
+    csv = subject.new(upload_file)
     expect(csv.send(:parse_directory, 'asdf\asdf')).to eq('asdf/asdf')
   end
 
   it "removes the path  " do
-    csv = ParsePlaylistCsv.new(upload_file)
+    csv = subject.new(upload_file)
     expect(csv.send(:parse_directory, 'L:/Departmental/Digital Library Services/Private/StreamingAudio/')).to eq('')
   end
 
