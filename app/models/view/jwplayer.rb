496ccb5f3b52dcb3aa882a3c48aa57e6624c45d7
Jonathan Hartzler <Jonathan.Hartzler.3@nd.edu>
2014-09-22 15:19:18 -0400

    update jwplayer class to do both audio and video and playlists

--- app/models/view/jwplayer.rb
+++ app/models/view/jwplayer.rb
@@ -1,10 +1,10 @@
 class Jwplayer
   include RailsHelpers
 
-  attr_accessor :filename
+  attr_reader :media_playlist, :username, :ipaddress, :specific_token
 
-  def initialize(filename, username, ipaddress, specific_token = false)
-    @filename = filename
+  def initialize(media_playlist, username, ipaddress, specific_token = false)
+    @media_playlist = media_playlist
     @username = username
     @ipaddress = ipaddress
     @specific_token = specific_token
@@ -36,32 +36,107 @@ class Jwplayer
 
   private
 
+
     def default_options
-      {
+      opts = {
         fallback: false,
         primary: 'html5',
         id: 'jwplayer',
         html5player: '/assets/jwplayer.html5.js',
         flashplayer: '/assets/jwplayer.flash.swf',
-        autostart: true,
-        width: "100%",
-        aspectratio: "16:9",
-        playlist: [ { sources: sources} ]
+        playlist: sources,
+        androidhls: true,
+        logo: { file:"/assets/hesburgh.png", link:"http://library.nd.edu"},
       }
+
+      if video_playlist?
+        opts.merge(video_options)
+      else
+        opts.merge(audio_options)
+      end
+    end
+
+    def video_options
+      vo = {
+        autostart: true,
+        aspectratio: "16:9",
+        width: '80%'
+      }
+
+      if multiple?
+        vo.merge!( {
+        listbar: {
+          position: "right",
+          size: '20%',
+          layout: 'basic'
+        }})
+      end
+
+      vo
+    end
+
+
+    def audio_options
+      ao = {
+        autostart: false,
+      }
+
+      if multiple?
+        ao.merge!( {
+          width: 480,
+          height: 480,
+          listbar: {
+            position: "bottom",
+            size: '400',
+            layout: 'basic'
+        }})
+      else
+        ao.merge!( {
+          width: 480,
+          height: 56,
+        })
+      end
+
+      ao
+    end
+
+
+    def multiple?
+      media_playlist.rows.size > 1
+    end
+
+
+    def audio_playlist?
+      media_playlist.type == 'audio'
+    end
+
+
+    def video_playlist?
+      media_playlist.type == 'video'
     end
 
 
     def sources
-      [{
-          file: wowza_url_generator.html5,
-      },{
-          file: wowza_url_generator.rtmp
-      }]
+      ret = []
+
+      media_playlist.rows.each do | row |
+        ret << {
+          sources: [{
+              file: wowza_url_generator(row[:file]).rtmp
+          },{
+              file: wowza_url_generator(row[:file]).html5,
+          }],
+          title: row[:title]
+        }
+      end
+
+      ret
     end
 
 
-    def wowza_url_generator
-      @wowza_url_generator ||= WowzaUrlGenerator.new(filename, @username, @ipaddress, @specific_token)
+    def wowza_url_generator(filename)
+      @wowza_url_generator ||= WowzaUrlGenerator.new(filename, media_playlist.type, username, ipaddress, specific_token)
     end
+
 
 end
--- spec/models/service/jwplayer_spec.rb
+++ spec/models/service/jwplayer_spec.rb
@@ -1,20 +1,65 @@
 require 'spec_helper'
 
 describe Jwplayer do
-  let(:filename) { 'filename.mov' }
-  subject { described_class.new(filename, 'username', 'ipaddress') }
+  subject { described_class.new(media_playlist, 'username', 'ipaddress') }
 
-  before(:each) do
-    subject.stub(:sources).and_return([ "sources" ])
-    subject.stub(:rstp_url).and_return("http://url")
+  context "single audio" do
+    let(:rows) { [ { title: 'audio 1', file: 'C00139/C00139_0114.mp3' } ] }
+    let(:media_playlist) { double(MediaPlaylist, type: 'audio', rows: rows )}
+
+    before(:each) do
+      subject.stub(:rstp_url).and_return("http://url")
+    end
+
+    it "returns the jwplayer" do
+      expect(subject.jwplayer).to eq("<div id='jwplayer'>Loading the player...<h3>Troubleshooting</h3><ol><li>Check to see if you have <a href=\"http://get.adobe.com/flashplayer/\" target=\"_blank\">flash</a> installed.</li><li>We recommend using <a href=\"https://www.google.com/intl/en/chrome/browser/\" target=\"_blank\">Google Chrome</a></li></div><script type='text/javascript'>jwplayer('jwplayer').setup({\"fallback\":false,\"primary\":\"html5\",\"html5player\":\"/assets/jwplayer.html5.js\",\"flashplayer\":\"/assets/jwplayer.flash.swf\",\"playlist\":[{\"sources\":[{\"file\":\"rtmpt://wowza.library.nd.edu:1935/aod/_definst_/mp3:C00139/C00139_0114.mp3?t=ed53bc094d75bb9fe8a19ca1c6d64d7e\"},{\"file\":\"http://wowza.library.nd.edu:1935/aod/_definst_/mp3:C00139/C00139_0114.mp3/playlist.m3u8?t=ed53bc094d75bb9fe8a19ca1c6d64d7e\"}],\"title\":\"audio 1\"}],\"androidhls\":true,\"logo\":{\"file\":\"/assets/hesburgh.png\",\"link\":\"http://library.nd.edu\"},\"autostart\":false,\"width\":480,\"height\":56});</script>")
+    end
+
+    it "returns the rstp_link" do
+      expect(subject.rstp_link).to eq("<div id='jwplayer'><a href=\"http://url\">Play Video</a></div>")
+    end
   end
 
-  it "returns the jwplayer" do
-    expect(subject.jwplayer).to eq("<div id='jwplayer'>Loading the player...<h3>Troubleshooting</h3><ol><li>Check to see if you have <a href=\"http://get.adobe.com/flashplayer/\" target=\"_blank\">flash</a> installed.</li><li>We recommend using <a href=\"https://www.google.com/intl/en/chrome/browser/\" target=\"_blank\">Google Chrome</a></li></div><script type='text/javascript'>jwplayer('jwplayer').setup({\"fallback\":false,\"primary\":\"html5\",\"html5player\":\"/assets/jwplayer.html5.js\",\"flashplayer\":\"/assets/jwplayer.flash.swf\",\"autostart\":true,\"width\":\"100%\",\"aspectratio\":\"16:9\",\"playlist\":[{\"sources\":[\"sources\"]}]});</script>")
+
+  context "double audio" do
+    let(:rows) { [ { title: 'audio 1', file: 'C00139/C00139_0114.mp3' }, { title: 'audio2', file: 'C00139/C00139_0114.mp3' } ] }
+    let(:media_playlist) { double(MediaPlaylist, type: 'audio', rows: rows )}
+
+    it "returns the jwplayer" do
+      expect(subject.jwplayer).to eq("<div id='jwplayer'>Loading the player...<h3>Troubleshooting</h3><ol><li>Check to see if you have <a href=\"http://get.adobe.com/flashplayer/\" target=\"_blank\">flash</a> installed.</li><li>We recommend using <a href=\"https://www.google.com/intl/en/chrome/browser/\" target=\"_blank\">Google Chrome</a></li></div><script type='text/javascript'>jwplayer('jwplayer').setup({\"fallback\":false,\"primary\":\"html5\",\"html5player\":\"/assets/jwplayer.html5.js\",\"flashplayer\":\"/assets/jwplayer.flash.swf\",\"playlist\":[{\"sources\":[{\"file\":\"rtmpt://wowza.library.nd.edu:1935/aod/_definst_/mp3:C00139/C00139_0114.mp3?t=efef6f62bfd1a773e5e5791e7d41b365\"},{\"file\":\"http://wowza.library.nd.edu:1935/aod/_definst_/mp3:C00139/C00139_0114.mp3/playlist.m3u8?t=efef6f62bfd1a773e5e5791e7d41b365\"}],\"title\":\"audio 1\"},{\"sources\":[{\"file\":\"rtmpt://wowza.library.nd.edu:1935/aod/_definst_/mp3:C00139/C00139_0114.mp3?t=efef6f62bfd1a773e5e5791e7d41b365\"},{\"file\":\"http://wowza.library.nd.edu:1935/aod/_definst_/mp3:C00139/C00139_0114.mp3/playlist.m3u8?t=efef6f62bfd1a773e5e5791e7d41b365\"}],\"title\":\"audio2\"}],\"androidhls\":true,\"logo\":{\"file\":\"/assets/hesburgh.png\",\"link\":\"http://library.nd.edu\"},\"autostart\":false,\"width\":480,\"height\":480,\"listbar\":{\"position\":\"bottom\",\"size\":\"400\",\"layout\":\"basic\"}});</script>")
+    end
+
+    it "returns the rstp_link" do
+      expect(subject.rstp_link).to eq("<div id='jwplayer'><a href=\"http://url\">Play Video</a></div>")
+    end
+
   end
 
-  it "returns the rstp_link" do
-    expect(subject.rstp_link).to eq("<div id='jwplayer'><a href=\"http://url\">Play Video</a></div>")
+
+  context "single video" do
+    let(:rows) { [ { title: 'video 1', file: 'filname.mov' } ] }
+    let(:media_playlist) { double(MediaPlaylist, type: 'video', rows: rows )}
+
+    it "returns the jwplayer" do
+      expect(subject.jwplayer).to eq("<div id='jwplayer'>Loading the player...<h3>Troubleshooting</h3><ol><li>Check to see if you have <a href=\"http://get.adobe.com/flashplayer/\" target=\"_blank\">flash</a> installed.</li><li>We recommend using <a href=\"https://www.google.com/intl/en/chrome/browser/\" target=\"_blank\">Google Chrome</a></li></div><script type='text/javascript'>jwplayer('jwplayer').setup({\"fallback\":false,\"primary\":\"html5\",\"html5player\":\"/assets/jwplayer.html5.js\",\"flashplayer\":\"/assets/jwplayer.flash.swf\",\"playlist\":[{\"sources\":[{\"file\":\"rtmpt://wowza.library.nd.edu:1935/vod/mp4:filname.mov?t=0379efd60d4b5d5a5ace08c2d40d8614\"},{\"file\":\"http://wowza.library.nd.edu:1935/vod/mp4:filname.mov/playlist.m3u8?t=0379efd60d4b5d5a5ace08c2d40d8614\"}],\"title\":\"video 1\"}],\"androidhls\":true,\"logo\":{\"file\":\"/assets/hesburgh.png\",\"link\":\"http://library.nd.edu\"},\"autostart\":true,\"aspectratio\":\"16:9\",\"width\":\"80%\"});</script>")
+    end
+
+    it "returns the rstp_link" do
+      expect(subject.rstp_link).to eq("<div id='jwplayer'><a href=\"http://url\">Play Video</a></div>")
+    end
   end
 
+
+  context "double video" do
+    let(:rows) { [ { title: 'video 1', file: 'filname.mov' }, { title: 'video 2', file: 'filename2' } ] }
+    let(:media_playlist) { double(MediaPlaylist, type: 'video', rows: rows )}
+
+    it "returns the jwplayer" do
+      expect(subject.jwplayer).to eq("<div id='jwplayer'>Loading the player...<h3>Troubleshooting</h3><ol><li>Check to see if you have <a href=\"http://get.adobe.com/flashplayer/\" target=\"_blank\">flash</a> installed.</li><li>We recommend using <a href=\"https://www.google.com/intl/en/chrome/browser/\" target=\"_blank\">Google Chrome</a></li></div><script type='text/javascript'>jwplayer('jwplayer').setup({\"fallback\":false,\"primary\":\"html5\",\"html5player\":\"/assets/jwplayer.html5.js\",\"flashplayer\":\"/assets/jwplayer.flash.swf\",\"playlist\":[{\"sources\":[{\"file\":\"rtmpt://wowza.library.nd.edu:1935/vod/mp4:filname.mov?t=32893b773ce3775fac6b9a6d55dea4c6\"},{\"file\":\"http://wowza.library.nd.edu:1935/vod/mp4:filname.mov/playlist.m3u8?t=32893b773ce3775fac6b9a6d55dea4c6\"}],\"title\":\"video 1\"},{\"sources\":[{\"file\":\"rtmpt://wowza.library.nd.edu:1935/vod/mp4:filname.mov?t=32893b773ce3775fac6b9a6d55dea4c6\"},{\"file\":\"http://wowza.library.nd.edu:1935/vod/mp4:filname.mov/playlist.m3u8?t=32893b773ce3775fac6b9a6d55dea4c6\"}],\"title\":\"video 2\"}],\"androidhls\":true,\"logo\":{\"file\":\"/assets/hesburgh.png\",\"link\":\"http://library.nd.edu\"},\"autostart\":true,\"aspectratio\":\"16:9\",\"width\":\"80%\",\"listbar\":{\"position\":\"right\",\"size\":\"20%\",\"layout\":\"basic\"}});</script>")
+    end
+
+    it "returns the rstp_link" do
+      expect(subject.rstp_link).to eq("<div id='jwplayer'><a href=\"http://url\">Play Video</a></div>")
+    end
+  end
 end
