280153df248f3b9bab128d32117dbe5ffeb4a388
Jonathan Hartzler <Jonathan.Hartzler.3@nd.edu>
2014-09-23 09:59:09 -0400

    change to allow the get reserve to play the media playlist.

--- app/models/policy/reserve_requires_terms_of_service_agreement.rb
+++ app/models/policy/reserve_requires_terms_of_service_agreement.rb
@@ -7,7 +7,7 @@ class ReserveRequiresTermsOfServiceAgreement
 
   def requires_agreement?
     policy = ElectronicReservePolicy.new(@reserve)
-    policy.has_file_resource? || policy.has_streaming_resource?
+    policy.has_file_resource? || policy.has_streaming_resource? || policy.has_media_playlist?
   end
 
 end
