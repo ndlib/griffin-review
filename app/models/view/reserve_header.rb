c78fbdbe1960913225a65072e54eb903f1f597cc
Jonathan Hartzler <Jonathan.Hartzler.3@nd.edu>
2014-09-23 09:59:38 -0400

    change the public display of the media playlist.

--- app/models/view/reserve_header.rb
+++ app/models/view/reserve_header.rb
@@ -1,16 +1,23 @@
 class ReserveHeader
   include RailsHelpers
 
+  attr_accessor :reserve
+  delegate :id, :file, :url, :title, :length, :course , :details, :publisher_provider, :realtime_availability_id, to: :reserve
+
   def initialize(reserve)
     @reserve = reserve
   end
 
   def title
-    ERB::Util.h(@reserve.title)
+    ERB::Util.h(reserve.title)
+  end
+
+  def creator_contributor
+    "#{@reserve.creator_contributor}#{(@reserve.creator_contributor.present? ? '<br>' : '')}"
   end
 
   def course_link
-    helpers.link_to @reserve.course.title, routes.course_reserves_path(@reserve.course.id)
+    helpers.link_to reserve.course.title, routes.course_reserves_path(reserve.course.id)
   end
 
 end
