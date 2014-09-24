76eb6bd642881f49854bc040c122cf73aaabb526
Jonathan Hartzler <Jonathan.Hartzler.3@nd.edu>
2014-09-24 09:48:50 -0400

    attempt to test the token sharing against pprd.

--- app/models/service/wowza_token_generator.rb
+++ app/models/service/wowza_token_generator.rb
@@ -12,7 +12,7 @@ class WowzaTokenGenerator
   def generate(username, ip)
     return @database_token if @database_token
 
-    @database_token = WowzaToken.where( username: username, ip: ip, token: hashed_token).first
+    @database_token = WowzaToken.where( username: username, ip: ip).first
     @database_token ||= WowzaToken.create( username: username, ip: ip, token: hashed_token, timestamp: Time.now.to_i )
   end
 
