b86515814e051835025828c545698baf90f5f290
Jonathan Hartzler <Jonathan.Hartzler.3@nd.edu>
2014-09-25 14:23:29 -0400

    token change

--- app/models/service/wowza_token_generator.rb
+++ app/models/service/wowza_token_generator.rb
@@ -12,17 +12,17 @@ class WowzaTokenGenerator
   def generate(username, ip)
     return @database_token if @database_token
     @database_token = WowzaToken.where( username: username, ip: ip).first
-    @database_token ||= WowzaToken.create( username: username, ip: ip, token: hashed_token, unhashed_token: token, timestamp: Time.now.to_i )
+    @database_token ||= WowzaToken.create( username: username, ip: ip, token: hashed_token, unhashed_token: token(username), timestamp: Time.now.to_i )
 
     token(username)
   end
 
-  def token(username)
-    @token = Digest::MD5.hexdigest(username)
-    # @token ||= SecureRandom.hex
-  end
-
   private
+
+    def token(username)
+      @token = Digest::MD5.hexdigest(username)
+      # @token ||= SecureRandom.hex
+    end
 
     def hashed_token
       Digest::MD5.hexdigest(token)
