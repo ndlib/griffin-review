0b71ea7bb150925a463da14df99bf4ef8e43e5e3
Jonathan Hartzler <Jonathan.Hartzler.3@nd.edu>
2014-09-25 13:26:13 -0400

    update token generator to try and get the query to work on the wowza end.

--- app/models/service/wowza_token_generator.rb
+++ app/models/service/wowza_token_generator.rb
@@ -7,18 +7,17 @@ class WowzaTokenGenerator
   def self.generate(username, ip)
     obj = new()
     obj.generate(username, ip)
-
-    obj.token
   end
 
   def generate(username, ip)
     return @database_token if @database_token
     @database_token = WowzaToken.where( username: username, ip: ip).first
     @database_token ||= WowzaToken.create( username: username, ip: ip, token: hashed_token, unhashed_token: token, timestamp: Time.now.to_i )
-    @database_token
+
+    token(username)
   end
 
-  def token
+  def token(username)
     @token = Digest::MD5.hexdigest(username)
     # @token ||= SecureRandom.hex
   end
