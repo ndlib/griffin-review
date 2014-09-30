6c2c8846203c45d2ab555b3969ca45055ccb8764
Jonathan Hartzler <Jonathan.Hartzler.3@nd.edu>
2014-09-30 10:18:24 -0400

    try %20

--- app/models/service/wowza_url_generator.rb
+++ app/models/service/wowza_url_generator.rb
@@ -29,7 +29,7 @@ class WowzaUrlGenerator
     end
 
     def filename
-      "#{base_directory}#{@filename.gsub(' ', '+')}"
+      "#{base_directory}#{@filename.gsub(' ', '%20')}"
     end
 
     def token
