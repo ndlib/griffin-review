3a6def1ec4a6f8d62aff60c3eb7b5800c8a6158d
Jonathan Hartzler <Jonathan.Hartzler.3@nd.edu>
2014-10-02 11:42:52 -0400

    add the timestamp update to reclaim the token.

--- app/models/service/wowza_token_generator.rb
+++ app/models/service/wowza_token_generator.rb
@@ -11,8 +11,12 @@ class WowzaTokenGenerator
 
   def generate(username, ip)
     return @database_token if @database_token
-    @database_token = WowzaToken.where( username: username, ip: ip).first
-    @database_token ||= WowzaToken.create( username: username, ip: ip, token: hashed_token(username), timestamp: Time.now.to_i )
+    @database_token = WowzaToken.where( username: username, ip: ip).first || WowzaToken.create( username: username, ip: ip, token: hashed_token(username), timestamp: Time.now.to_i )
+
+    if @database_token.timestamp < 5.minutes.ago.to_i
+      @database_token.timestamp = Time.now.to_i
+      @database_token.save!
+    end
 
     token(username)
   end
