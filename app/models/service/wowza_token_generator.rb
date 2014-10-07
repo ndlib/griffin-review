bfbc6d4bd379d32846f8b945ea620c353670bb19
Jonathan Hartzler <Jonathan.Hartzler.3@nd.edu>
2014-10-07 09:48:54 -0400

    update the token generation to remove tokens when there is more then one in the database.

--- app/models/service/wowza_token_generator.rb
+++ app/models/service/wowza_token_generator.rb
@@ -3,32 +3,51 @@ require 'securerandom'
 
 
 class WowzaTokenGenerator
+  attr_reader :database_token, :username, :ip
 
   def self.generate(username, ip)
     obj = new()
-    obj.generate(username, ip)
+    obj.generate
   end
 
-  def generate(username, ip)
-    return @database_token if @database_token
-    @database_token = WowzaToken.where( username: username, ip: ip).first || WowzaToken.create( username: username, ip: ip, token: hashed_token(username), timestamp: Time.now.to_i )
+  def initialize(username, ip)
+    @username = username
+    @ip = ip
+  end
 
-    if @database_token.timestamp < 5.minutes.ago.to_i
-      @database_token.timestamp = Time.now.to_i
-      @database_token.save!
-    end
+  def generate
+    return database_token if database_token
 
-    token(username)
+    build_database_token
+    verify_token_timestamp
+
+    token
   end
 
   private
 
-    def token(username)
+    def token
       Digest::MD5.hexdigest(username)
     end
 
-    def hashed_token(username)
-      Digest::MD5.hexdigest(token(username))
+    def hashed_token
+      Digest::MD5.hexdigest(token)
     end
 
+    def build_database_token
+      tokens = WowzaToken.where(username: username, ip: ip)
+      if tokens.size > 1
+        tokens.destroy_all
+      end
+
+      @database_token = tokens.first || WowzaToken.create( username: username, ip: ip, token: hashed_token, timestamp: Time.now.to_i )
+
+    end
+
+    def verify_token_timestamp
+      if database_token.timestamp < 5.minutes.ago.to_i
+        database_token.timestamp = Time.now.to_i
+        database_token.save!
+      end
+    end
 end
