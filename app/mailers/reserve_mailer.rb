2ffd0d6d231878eae2e12a8de1a88ef41080dd65
Robert Fox <robertjonfox@gmail.com>
2014-10-16 09:54:56 -0400

    Just added some spacing

--- app/mailers/reserve_mailer.rb
+++ app/mailers/reserve_mailer.rb
@@ -11,14 +11,15 @@ class ReserveMailer < ActionMailer::Base
 
 
   private
-    def determine_email_for_reserve(reserve)
-      if ['VideoReserve'].include?(reserve.type)
-        'prader@nd.edu'
-      elsif ['AudioReserve'].include?(reserve.type)
-        'Robert.C.Simon.41@nd.edu'
-      else
-        'reserves.1@nd.edu'
-      end
+
+  def determine_email_for_reserve(reserve)
+    if ['VideoReserve'].include?(reserve.type)
+      'prader@nd.edu'
+    elsif ['AudioReserve'].include?(reserve.type)
+      'Robert.C.Simon.41@nd.edu'
+    else
+      'reserves.1@nd.edu'
     end
+  end
 
 end
