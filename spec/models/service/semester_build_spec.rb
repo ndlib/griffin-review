dff3f9bcd2ec74b7a7b45274557a89aca77d2da5
Robert Fox <robertjonfox@gmail.com>
2014-10-16 09:55:49 -0400

    Accomodate tests to new code

--- spec/models/service/semester_build_spec.rb
+++ spec/models/service/semester_build_spec.rb
@@ -8,11 +8,16 @@ describe SemesterBuild do
 
   describe "#find_next_semester" do
 
+    before(:each) do
+      s = Semester.new(code: '11111', full_name: 'Fall Semester', date_begin: Date.today, date_end: Date.today + 15)
+      s.save!
+    end
+
     context "when the next semester is found in the calendar" do
       it "should create a new active record semester" do
         SemesterCalendarFetch.any_instance.stub(:fetch_calendar).and_return(full_calendar)
         subject.find_next_semester
-        expect(Semester.count).to eq 1
+        expect(Semester.count).to eq 2
         expect(Semester.last.code).to eq '201410'
         expect(Semester.last.full_name).to eq 'Fall 2014'
         expect(Semester.last.date_begin.to_s).to eq '08/26/2014'
@@ -22,9 +27,10 @@ describe SemesterBuild do
     context "when the next semester is not found in the calendar" do
       it "should not create a new active record semester" do
         SemesterCalendarFetch.any_instance.stub(:fetch_calendar).and_return(full_calendar_sans_semester)
+        expect(subject).to receive(:past_drop_dead_date?).and_return(false).at_least(:once)
         subject.find_next_semester
-        expect(Semester.count).to eq 0
-        expect(Semester.last).to be_nil
+        expect(Semester.count).to eq 1
+        expect(Semester.last.code).to eq '11111'
       end
     end
 
