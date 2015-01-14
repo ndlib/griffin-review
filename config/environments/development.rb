f4fa0a21cd972a6564136cc951c32c1eeaed14f4
Jonathan Hartzler <Jonathan.Hartzler.3@nd.edu>
2015-01-14 16:27:59 -0500

    fix typo

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -71,6 +71,9 @@ Griffin::Application.configure do
   config.api_url                        = "https://api2pprd-vm.library.nd.edu"
   config.api_token                      = "SCSGYmwqLqSyBTmxCQgM"
 
+  config.api_url                        = "https://api2prod-vm.library.nd.edu"
+  config.api_token                      = "nDKg6F8dAcUkCdzzFqc5"
+
   #config.api_url                          = "http://localhost:3005/"
   #config.api_token                        = "jz4LC72U6nXAKLx6Vssx"
 
