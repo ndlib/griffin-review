a6647172254579337e6eb5e7e5494e0cd1479c35
Jonathan Hartzler <Jonathan.Hartzler.3@nd.edu>
2015-01-14 09:29:30 -0500

    change the api locations

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -68,10 +68,11 @@ Griffin::Application.configure do
   config.api_url                          = "https://api.library.nd.edu/"
   config.api_token                        = "nDKg6F8dAcUkCdzzFqc5"
 
+  config.api_url                        = "https://api2pprd-vm.library.nd.edu"
+  config.api_token                      = "SCSGYmwqLqSyBTmxCQgM"
+
   #config.api_url                          = "http://localhost:3005/"
   #config.api_token                        = "jz4LC72U6nXAKLx6Vssx"
-
-
 
   config.path_to_old_files                = File.join(Rails.root, 'uploads', 'old_files')
 
