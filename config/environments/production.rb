fdc470a5f3333f79ecc9f731528bba08c5206137
Jonathan Hartzler <Jonathan.Hartzler.3@nd.edu>
2015-01-23 09:42:46 -0500

    update the environment to test api again.

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -79,8 +79,8 @@ Griffin::Application.configure do
   config.api_url                        = "https://api.library.nd.edu/"
   config.api_token                      = "nDKg6F8dAcUkCdzzFqc5"
 
-#  config.api_url                        = "https://api2prod-vm.library.nd.edu"
-#  config.api_token                      = "nDKg6F8dAcUkCdzzFqc5"
+  config.api_url                        = "https://api2prod-vm.library.nd.edu"
+  config.api_token                      = "nDKg6F8dAcUkCdzzFqc5"
 
   config.path_to_old_files              = File.join(Rails.root, 'uploads', 'old_files')
 
