3f5ccc66688d21d4a9baf61891a87b2b6091c2a8
Jonathan Hartzler <Jonathan.Hartzler.3@nd.edu>
2015-03-31 10:59:46 -0400

    fix issue with csrf breaking a redirect

--- app/controllers/sakai_integrator_controller.rb
+++ app/controllers/sakai_integrator_controller.rb
@@ -1,9 +1,9 @@
 class SakaiIntegratorController < ApplicationController
-  include GetCourse
+  protect_from_forgery :except => :sakai_redirect
+
 
   def sakai_redirect
     course = SakaiIntegrator.call(params[:context_id], current_user.username)
-
     if course
       redirect_to '/sakai' + course_reserves_path(course.id)
     else
