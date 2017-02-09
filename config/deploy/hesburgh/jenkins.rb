60b2027d758c1f183a551b93b1165215524ec0f9
Jonathan Hartzler <Jonathan.Hartzler.3@nd.edu>
2017-02-09 13:38:34 -0500

    test with out this config

--- config/deploy/hesburgh/jenkins.rb
+++ config/deploy/hesburgh/jenkins.rb
@@ -1,6 +1,6 @@
 Capistrano::Configuration.instance(:must_exist).load do
 
   ssh_options[:paranoid] = false
-  ssh_options[:keys] = %w(/opt/jenkins/.ssh/id_rsa)
+#  ssh_options[:keys] = %w(/opt/jenkins/.ssh/id_rsa)
 
 end
