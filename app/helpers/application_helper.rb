217a5de7c3de29cdeabff7b0bee0d43506b06ec7
Dan Wolfe <dwolfe2@nd.edu>
2017-08-23 12:15:20 -0400

    switch default player to html5

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -77,7 +77,7 @@ module ApplicationHelper
         }
       ]
 
-      options = { fallback: false, primary: 'flash', id: 'jwplayer', html5player: '/assets/jwplayer.html5.js', flashplayer: '/assets/jwplayer.flash.swf', autostart: true, sources: sources }.merge(options)
+      options = { fallback: false, primary: 'html5', id: 'jwplayer', html5player: '/assets/jwplayer.html5.js', flashplayer: '/assets/jwplayer.flash.swf', autostart: true, sources: sources }.merge(options)
 
       result = %Q{<div id='#{options[:id]}'>Loading the player...</div>
                   <script type='text/javascript'>
