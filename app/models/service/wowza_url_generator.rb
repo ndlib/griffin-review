725613acc56dedf48b5ed74b16091db17338d989
Dan Wolfe <dwolfe2@nd.edu>
2017-08-25 13:30:07 -0400

    M4A

--- app/models/service/wowza_url_generator.rb
+++ app/models/service/wowza_url_generator.rb
@@ -48,7 +48,7 @@ class WowzaUrlGenerator
 
     def base_directory
       if @type == 'audio'
-        'mp3:StreamingAudio/MP3/'
+        'mp4:StreamingAudio/MP4/'
       elsif @type == 'video'
         'mp4:All Streamed Videos/'
       else
