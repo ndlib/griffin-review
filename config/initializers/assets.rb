d240a68c215703972301984aff98984e0eba5db2
Ryan Doughty <rdought1@nd.edu>
2019-10-24 12:32:24 -0400

    additional stylesheets to precompile

--- config/initializers/assets.rb
+++ config/initializers/assets.rb
@@ -1,3 +1,3 @@
 Rails.application.config.assets.precompile += %w( hesburgh_assets/admin/1.0/index.css )
 Rails.application.config.assets.precompile += %w( hesburgh_assets/admin/1.0/index.js )
-Rails.application.config.assets.precompile += %w( app/assets/stylesheets/*.css )
+Rails.application.config.assets.precompile += %w( search.css sakai.css reserves.css print.css datatables.custom.css admin-requests.css admin-playlist-form.css )
