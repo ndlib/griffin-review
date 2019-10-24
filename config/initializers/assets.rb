7f232e2713c5062ab26c4c90f1202e19ace6e4d6
Ryan Doughty <rdought1@nd.edu>
2019-10-24 12:23:50 -0400

    specify which css to precompile

--- config/initializers/assets.rb
+++ config/initializers/assets.rb
@@ -1,3 +1,3 @@
 Rails.application.config.assets.precompile += %w( hesburgh_assets/admin/1.0/index.css )
 Rails.application.config.assets.precompile += %w( hesburgh_assets/admin/1.0/index.js )
-Rails.application.config.assets.precompile += %w( *.css *.js )
+Rails.application.config.assets.precompile += %w( app/assets/stylesheets/*.css )
