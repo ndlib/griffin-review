a86d5c7e379096bf3837c9782a9a6199d2d2f490
Ryan Doughty <rdought1@nd.edu>
2020-05-07 15:37:23 -0400

    email course primary instructor not the requestor

--- app/models/service/notify_reserve_requestor.rb
+++ app/models/service/notify_reserve_requestor.rb
@@ -11,18 +11,24 @@ class NotifyReserveRequestor
     params[:time_delta] = Time.now - 24.hours
     params[:state_type] = 'available'
     params[:item_type] = 'VideoReserve'
-    Request.select(:item_title, :requestor_netid)
+    Request.select(:id, :item_title, :course_id)
     .where("workflow_state_change_date >= ? AND workflow_state = ? AND item_type = ?", params[:time_delta], params[:state_type], params[:item_type])
-    .collect do |r|
-      netid = r.requestor_netid
-      if @email_info.key?(netid)
-        @email_info[netid]['requests'] << r.item_title
+    .collect do |req|
+      # email the course primary instructor and not the request requestor
+      # since staff enter some request on behalf of the instructor
+      course = CourseSearch.new.get(req.course_id)
+      if course.primary_instructor
+        netid = course.primary_instructor.username
+        if @email_info.key?(netid)
+          @email_info[netid]['requests'] << req.item_title
+        else
+          @email_info[netid] = Hash.new
+          @email_info[netid]['email'] = course.primary_instructor.email
+          @email_info[netid]['display_name'] = course.primary_instructor.display_name
+          @email_info[netid]['requests'] = [req.item_title]
+        end
       else
-        @user = User.where(:username => netid).first
-        @email_info[netid] = Hash.new
-        @email_info[netid]['email'] = @user.email
-        @email_info[netid]['display_name'] = @user.display_name
-        @email_info[netid]['requests'] = [r.item_title]
+        p "ERROR: No primary instructor found for request id " + req.id.to_s + " belonging to course id " + req.course_id
       end
     end
     ReserveMailer.published_request_notifier(@email_info).deliver_now
