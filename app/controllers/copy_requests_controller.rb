03891066b0816bd65b3967ec1b6b25ec020e94d2
Ryan Doughty <rdought1@nd.edu>
2020-09-09 15:50:22 -0400

    pass copy flag

--- app/controllers/copy_requests_controller.rb
+++ app/controllers/copy_requests_controller.rb
@@ -3,7 +3,7 @@ class CopyRequestsController < ApplicationController
     reserve = ReserveSearch.new.get(params[:id])
     course = CourseSearch.new.get(reserve.course_id)
     if reserve && reserve.can_copy_reserve?
-      res = CopyReserve.new(current_user, course, reserve).copy
+      res = CopyReserve.new(current_user, course, reserve).copy(true)
       flash[:success] = 'Request Copy Success - This is the requested copy.'
       redirect_to request_path(res.id)
     else
