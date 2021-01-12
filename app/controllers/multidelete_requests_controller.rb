b4fbc63d4636b8c45f51021f7e9116199ada4426
Ryan Doughty <rdought1@nd.edu>
2021-01-12 10:11:19 -0500

    prevent error when user deletes without selections

--- app/controllers/multidelete_requests_controller.rb
+++ app/controllers/multidelete_requests_controller.rb
@@ -1,8 +1,18 @@
 class MultideleteRequestsController < ApplicationController
   def destroy
     check_instructor_permissions!(CourseSearch.new.get(params['course_id']))
-    params[:request_ids].each do |id|
-      Request.update(id, workflow_state: 'removed')
+    num_ids = 0
+    if params[:request_ids].present?
+      num_ids = params[:request_ids].length
+      params[:request_ids].each do |id|
+        Request.update(id, workflow_state: 'removed')
+      end
+    end
+
+    if num_ids > 0
+      flash[:success] = "Removed #{num_ids} request(s)."
+    else
+      flash[:notice] = "Please select request(s) to remove and try again"
     end
     redirect_to course_reserves_path(params['course_id'])
   end
