9a7cacb3a821a29a0dce5392237f2022356232b7
Ryan Doughty <rdought1@nd.edu>
2021-01-15 10:21:06 -0500

    fix update so CI can work

--- db/migrate/20131217151531_update_requests_to_list_reviewed.rb
+++ db/migrate/20131217151531_update_requests_to_list_reviewed.rb
@@ -1,8 +1,8 @@
 class UpdateRequestsToListReviewed < ActiveRecord::Migration
   def change
 
-    add_column :requests, :reviewed, :boolean
+    # add_column :requests, :reviewed, :boolean
 
-    Request.update_all('reviewed = 1', 'workflow_state != "new" ')
+    Request.update_all "reviewed = 1 where workflow_state != 'new'"
   end
 end
