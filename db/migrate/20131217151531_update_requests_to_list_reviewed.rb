76513b62bf64ef07371d7e5600ec26316718de9e
Ryan Doughty <rdought1@nd.edu>
2021-01-15 11:14:50 -0500

    readd reviewed col

--- db/migrate/20131217151531_update_requests_to_list_reviewed.rb
+++ db/migrate/20131217151531_update_requests_to_list_reviewed.rb
@@ -1,7 +1,7 @@
 class UpdateRequestsToListReviewed < ActiveRecord::Migration
   def change
 
-    # add_column :requests, :reviewed, :boolean
+    add_column :requests, :reviewed, :boolean
 
     Request.update_all "reviewed = 1 where workflow_state != 'new'"
   end
