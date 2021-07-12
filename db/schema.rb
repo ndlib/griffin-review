fb8585bdbed8ad7fe9f19b199a6a1fc4514c4e5c
Ryan Doughty <rdought1@nd.edu>
2021-07-12 19:15:32 -0400

    setting video_titles encoding

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20200422171148) do
+ActiveRecord::Schema.define(version: 20210712222106) do
 
   create_table "assignments", force: :cascade do |t|
     t.integer  "user_id",    limit: 4
