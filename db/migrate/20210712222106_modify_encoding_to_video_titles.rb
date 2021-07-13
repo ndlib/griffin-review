d56326951e284cce0d6c76b10b9257f2a9e92c24
Ryan Doughty <rdought1@nd.edu>
2021-07-12 21:42:35 -0400

    removed database name

--- db/migrate/20210712222106_modify_encoding_to_video_titles.rb
+++ db/migrate/20210712222106_modify_encoding_to_video_titles.rb
@@ -1,9 +1,9 @@
 class ModifyEncodingToVideoTitles < ActiveRecord::Migration
   def self.up
-    execute "ALTER TABLE reserves_development.video_titles MODIFY name VARCHAR(255) CHARACTER SET utf8 COLLATE utf8_unicode_ci;"
+    execute "ALTER TABLE video_titles MODIFY name VARCHAR(255) CHARACTER SET utf8 COLLATE utf8_unicode_ci;"
   end
 
   def self.down
-    execute "ALTER TABLE reserves_development.video_titles MODIFY name VARCHAR(255) CHARACTER SET latin1;"
+    execute "ALTER TABLE video_titles MODIFY name VARCHAR(255) CHARACTER SET latin1;"
   end
 end
